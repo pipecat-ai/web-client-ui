df4dfa29ed567145f3b6fbe37bea93fe03be2150
Aleix Conchillo Flaqué <aleix@daily.co>
2024-06-14 14:50:27 -0700

    super simple VAD

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import { useState } from "react";
-import { useDaily } from "@daily-co/daily-react";
+import { useCallback, useState } from "react";
+import { useDaily, useDailyEvent } from "@daily-co/daily-react";
 import { ArrowRight, Loader2 } from "lucide-react";
 
 import { Alert } from "./components/alert";
@@ -120,6 +120,83 @@ export default function App() {
     await daily?.leave();
     setState("idle");
   }
+
+  function currentTime() {
+    return Date.now() / 1000;
+  }
+
+  function handleVAD(type, track) {
+      const audioContext = new AudioContext();
+
+      const stream = new MediaStream([track]);
+      const source = audioContext.createMediaStreamSource(stream);
+
+      const bufferSize = 1024; // For 44100 this would be like 10ms
+      const scriptProcessor = audioContext.createScriptProcessor(bufferSize, 1, 1);
+
+      source.connect(scriptProcessor);
+      scriptProcessor.connect(audioContext.destination);
+
+      const speechThreshold = 0.03;
+      const smoothingFactor = 0.2;
+      const vadStartSecs = 0.2;
+      const vadStopSecs = 0.8;
+
+      let prevRMS = 0;
+      let prevState = 'silence';
+      let lastTimeSpeaking = 0;
+      let lastTimeSilence = currentTime();
+
+      // TODO(aleix): We need to stop this when the meeting ends.
+      scriptProcessor.onaudioprocess = (audioProcessingEvent) => {
+        const inputBuffer = audioProcessingEvent.inputBuffer;
+        const inputData = inputBuffer.getChannelData(0); // Assuming mono audio
+        let sum = 0;
+
+        // Compute the sum of squares of the samples
+        for (let i = 0; i < inputData.length; i++) {
+          sum += inputData[i] * inputData[i];
+        }
+
+        const rms = Math.sqrt(sum / inputData.length);
+        const smoothedRMS = prevRMS + smoothingFactor * (rms - prevRMS);
+
+        let speaking = smoothedRMS >= speechThreshold;
+
+       const currTime = currentTime();
+        if (speaking) {
+          if (prevState === 'silence') {
+            const speakingTime = currTime - lastTimeSilence;
+            if (speakingTime >= vadStartSecs) {
+              console.log(`${type} STARTED SPEAKING`);
+              prevState = 'speaking';
+            }
+          }
+          lastTimeSpeaking = currentTime();
+        } else {
+          if (prevState === 'speaking') {
+            const silenceTime = currTime - lastTimeSpeaking;
+            if (silenceTime >= vadStopSecs) {
+              console.log(`${type} STOPPED SPEAKING`);
+              prevState = 'silence';
+            }
+          }
+          lastTimeSilence = currentTime();
+        }
+
+        prevRMS = smoothedRMS;
+      }
+  }
+  useDailyEvent(
+    'track-started',
+    useCallback((ev) => {
+      if (ev.participant?.local) {
+        handleVAD('local', ev.track);
+      } else {
+        handleVAD('remote', ev.track);
+      }
+    }, [])
+  );
 
   if (state === "error") {
     return (
